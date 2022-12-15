7ec16d0c7e7978a494cee2563348ec977320d166
zxczxc123 <magtrim12@naver.com>
2022-12-15 14:56:41 +0900

    Update 1.variable.js

--- 1.variable.js
+++ 1.variable.js
@@ -20,3 +20,20 @@ let orange = apple;
 
 orange.name = 'orange';
 console.log(apple);
+
+// let vs const
+// let 재할당 가능
+
+
+// const 재할당 불가 
+// 1. 상수
+const MAX_FRUITS = 5;
+// 2. 재할당 불가 상수변수 또는 변수
+const cake = {
+  name: 'apple',
+  color: 'red',
+  display: '🍎',
+};
+
+// let 재할당가능 , 객체내 변경가능
+// const 재할당불가 , 객체내 변경가능
